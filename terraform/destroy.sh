4d149ff866cc9375bef6a86a49d5ac2e37df4178
Charles Lee <cleeistaken@gmail.com>
2022-04-01 23:19:15 -0500

    add destroy script

---
+++ terraform/destroy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+TERRAFORM="../config/terraform.tfvars"
+TERRAFORM_TEMPLATE="../config/terraform-template.tfvars"
+TERRAFORM_TFPLAN="tfplan"
+
+if [ ! -f "${TERRAFORM}" ]; then
+    echo "ERROR: The file ${TERRAFORM} is missing."
+    exit 1
+fi
+
+if [ ! -f "${TERRAFORM_TEMPLATE}" ]; then
+    echo "ERROR: The file ${TERRAFORM_TEMPLATE} is missing."
+    exit 1
+fi
+
+terraform destroy \
+-auto-approve \
+--var-file="${TERRAFORM}" \
+--var-file="${TERRAFORM_TEMPLATE}"
+
+if [ -f "${TERRAFORM_TFPLAN}" ]; then
+    echo "Removing tfplan file"
+    rm -f "${TERRAFORM_TFPLAN}"
+fi
