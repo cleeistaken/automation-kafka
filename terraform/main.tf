b5ba166bf2be9e2d8ca53f55a8eb77dbd8b16842
Charles Lee <cleeistaken@gmail.com>
2022-04-01 22:23:31 -0500

    adding inventory variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,10 +22,12 @@ module "template" {
   source = "./modules/template"
 
   # vSphere Cluster
-  vsphere_cluster = var.vsphere_cluster
+  vsphere_datacenter = var.vsphere_datacenter
+  vsphere_datastore = var.vsphere_datastore
 
   # Template
-  content_library_item_url = format("http://%s/%s", data.external.local_ip.result.ip, var.template_ova)
+  content_library_name = var.template_name
+  content_library_item_url = format("http://%s/templates/%s", data.external.local_ip.result.ip, var.template_ova)
   content_library_item_name = var.template_name
   content_library_item_description = var.template_description
 }
@@ -36,28 +38,44 @@ module "template" {
 module "kafka" {
   source = "./modules/kafka"
 
-  # Clusters
-  vsphere_cluster = var.vsphere_cluster
+  # vSphere
+  vsphere_datacenter = var.vsphere_datacenter
+  vsphere_compute_cluster = var.vsphere_compute_cluster
+  vsphere_folder_vm = var.vsphere_folder_vm
+  vsphere_resource_pool = var.vsphere_resource_pool
+  vsphere_network_1_portgroup = var.vsphere_network_1_portgroup
+  vsphere_network_1_ipv4_subnet_cidr = var.vsphere_network_1_ipv4_subnet_cidr
+  vsphere_network_1_ipv4_ips = var.vsphere_network_1_ipv4_ips
+  vsphere_network_1_ipv4_gateway = var.vsphere_network_1_ipv4_gateway
+  vsphere_network_2_portgroup = var.vsphere_network_1_portgroup
+  vsphere_network_2_ipv4_subnet_cidr = var.vsphere_network_1_ipv4_subnet_cidr
+  vsphere_network_2_ipv4_ips = var.vsphere_network_1_ipv4_ips
+  vsphere_datastore = var.vsphere_datastore
+
+  # Network
+  network_domain_name = var.network_domain_name
+  network_ipv4_dns_servers = var.network_ipv4_dns_servers
+  network_dns_suffix = var.network_dns_suffix
 
   # Template
   template = module.template.content_library_item
   template_boot = var.template_boot
 
   # Prefix for all VM
-  kafka_vm_prefix = var.kafka_vm_prefix
+  vm_kafka_prefix = var.vm_kafka_prefix
 
   # Control center
-  kafka_control_center = var.kafka_control_center
+  vm_kafka_control_center = var.vm_kafka_control_center
 
   # Broker
-  kafka_broker_count_per_cluster = var.kafka_broker_count_per_cluster
-  kafka_broker = var.kafka_broker
+  vm_kafka_broker_count_per_cluster = var.vm_kafka_broker_count_per_cluster
+  vm_kafka_broker = var.vm_kafka_broker
 
   # Zookeeper
-  kafka_zookeeper_count_per_cluster = var.kafka_zookeeper_count_per_cluster
-  kafka_zookeeper = var.kafka_zookeeper
+  vm_kafka_zookeeper_count_per_cluster = var.vm_kafka_zookeeper_count_per_cluster
+  vm_kafka_zookeeper = var.vm_kafka_zookeeper
 
   # Connect
-  kafka_connect_count_per_cluster = var.kafka_connect_count_per_cluster
-  kafka_connect = var.kafka_connect
+  vm_kafka_connect_count_per_cluster = var.vm_kafka_connect_count_per_cluster
+  vm_kafka_connect = var.vm_kafka_connect
 }
--- terraform/output.tf
+++ terraform/output.tf
@@ -9,6 +9,16 @@ output "kafka" {
 #
 module "inventory" {
   source  = "./modules/inventory"
+
+  # Environment information
   input = module.kafka
-  output_folder = "./"
+
+   # vCenter
+  vsphere_server = var.vcenter_server
+  user = var.vcenter_user
+  password = var.vcenter_password
+  allow_unverified_ssl = var.vcenter_insecure_ssl
+
+  # Output file location
+  output_folder = "../config"
 }
